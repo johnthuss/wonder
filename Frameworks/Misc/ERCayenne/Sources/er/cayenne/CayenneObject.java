efd4ac9b9e3fd8cfa0158b203b9882c44ce80694
John Huss <johnthuss@gmail.com>
2012-07-15 15:59:07 -0500

    Update Cayenne lib to 3.2-snapshot and make relevant changes for it
    Added some EOGenericRecord methods to CayenneObject
    Added deleteAllXXXRelationships to entity template

--- Frameworks/Misc/ERCayenne/Sources/er/cayenne/CayenneObject.java
+++ Frameworks/Misc/ERCayenne/Sources/er/cayenne/CayenneObject.java
@@ -1,6 +1,17 @@
 package er.cayenne;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.cayenne.Cayenne;
 import org.apache.cayenne.CayenneDataObject;
+import org.apache.cayenne.DataObject;
+import org.apache.cayenne.ObjectContext;
+import org.apache.cayenne.PersistenceState;
+import org.apache.cayenne.map.ObjEntity;
+import org.apache.cayenne.map.ObjRelationship;
+import org.apache.cayenne.map.Relationship;
+import org.apache.cayenne.validation.ValidationResult;
 
 import com.webobjects.foundation.NSKeyValueCoding;
 import com.webobjects.foundation.NSKeyValueCodingAdditions;
@@ -95,4 +106,72 @@ public class CayenneObject extends CayenneDataObject implements NSKeyValueCoding
 		super.writePropertyDirectly(propertyName, value);
 	}
 	
+	public ObjEntity entity() {
+		return Cayenne.getObjEntity(this);
+	}
+	
+    public String entityName() { 
+    	return entity().getName(); 
+    }
+    
+    public List<String> attributeKeys() { 
+    	return new ArrayList<String>(entity().getAttributeMap().keySet()); 
+    }
+
+	public List<String> toManyRelationshipKeys() {
+		List<String> result = new ArrayList<String>();
+		for (ObjRelationship relationship : entity().getRelationships()) {
+			if (relationship.isToMany()) {
+				if (!relationship.getName().startsWith("runtimeRelationship")) { // skip runtime generated relationships and just return the info defined in the model
+					result.add(relationship.getName());
+				}
+			}
+		}
+		return result;
+	}
+
+	public List<String> toOneRelationshipKeys() {
+		List<String> result = new ArrayList<String>();
+		for (ObjRelationship relationship : entity().getRelationships()) {
+			if (!relationship.isToMany()) {
+				if (!relationship.getName().startsWith("runtimeRelationship")) { // skip runtime generated relationships and just return the info defined in the model
+					result.add(relationship.getName());
+				}
+			}
+		}
+		return result;
+	}
+	
+	public List<String> allPropertyKeys() {
+		List<String> result = new ArrayList<String>();
+		result.addAll(attributeKeys());
+		result.addAll(toOneRelationshipKeys());
+		result.addAll(toManyRelationshipKeys());
+		return result;
+	}
+	
+	public boolean isReadOnly() {
+		return entity().isReadOnly();
+	}
+
+	public boolean isToManyKey(String key) {
+		Relationship relationship = entity().getRelationship(key);
+		return relationship != null && relationship.isToMany();
+	}
+
+	public boolean isFault() {
+		return getPersistenceState() == PersistenceState.HOLLOW;
+	}
+
+	public void willRead() {
+		if (getObjectContext() != null && !attributeKeys().isEmpty()) {
+			getObjectContext().prepareForAccess(this, attributeKeys().get(0), true);
+		}
+	}
+
+	public CayenneObject localInstanceIn(ObjectContext context) {
+		return context.localObject(this);
+	}
+	
 }
+
